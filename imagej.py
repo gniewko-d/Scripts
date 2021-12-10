aa88a91ae96b2941cf0ac71b1a99b6657d75a86a
TBlaszak <47916433+TBlaszak@users.noreply.github.com>
2021-12-10 14:49:26 +0100

    Update imagej.py

--- imagej.py
+++ imagej.py
@@ -20,6 +20,7 @@ import cv2
 import tifffile as tif
 #from gray2color import gray2color
 print ("Cycki są fajniejsze")
+print ("ale i tyłeczki rządzą")
 def gray2color(u,channel):
     """
     Compute color image from intensity in fluorescence in a given channel.
